cd9bd2550d11e932dafc233bc254eb69f146586e
yudai <fukuta.yudai@gmail.com>
2020-01-02 23:13:58 +0900

    add-configure_permitted_parameters
    新規登録時に、name、imageも登録可能に変更。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth, if: :production?
+  before_action :configure_permitted_parameters, if: :devise_controller?
  
   private
 
@@ -11,6 +12,12 @@ class ApplicationController < ActionController::Base
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :image])
   end
 
 end
