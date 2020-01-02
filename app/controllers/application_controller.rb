e96a2a69571cd703050b9bce089b7d0f9d5b2684
yudai <fukuta.yudai@gmail.com>
2020-01-03 00:42:14 +0900

    modify-indent

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,7 @@ class ApplicationController < ActionController::Base
 
   authorize_resource class: false
 
-  # 権限が無いページへアクセス時の例外処理
   rescue_from CanCan::AccessDenied do |exception|
-    # root_urlにかっ飛ばす。
     redirect_to root_url
   end
  
