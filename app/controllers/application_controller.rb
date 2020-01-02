c896053d2bb8ffa1eecce0e92adfab358fe4c36d
yudai <fukuta.yudai@gmail.com>
2020-01-03 00:32:31 +0900

    add-cancancan
    cancancanの導入。試験的に設定追加。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,14 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth, if: :production?
   before_action :configure_permitted_parameters, if: :devise_controller?
+
+  authorize_resource class: false
+
+  # 権限が無いページへアクセス時の例外処理
+  rescue_from CanCan::AccessDenied do |exception|
+    # root_urlにかっ飛ばす。
+    redirect_to root_url
+  end
  
   private
 
@@ -17,7 +25,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :image])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :image, :role])
   end
 
 end
