ff146a41585311a95fef744f765bd61d2c27c8c5
yudai <fukuta.yudai@gmail.com>
2020-01-10 16:01:18 +0900

    add-users-controller
    users_controllerを追加

--- app/controllers/family_posts_controller.rb
+++ app/controllers/family_posts_controller.rb
@@ -1,22 +1,18 @@
 class FamilyPostsController < ApplicationController
- def index
-  @family_post = FamilyPost.new
-  if current_user.role == "admin"
-    @family_posts = FamilyPost.all.order("created_at DESC").includes(:user)
-  else
-    @family_posts = FamilyPost.where(user_id: current_user.id).order("created_at DESC").includes(:user)
-  end
-end
+#  def index
+#   @family_post = FamilyPost.new
+#   @family_posts = FamilyPost.where(user_id: current_user.id).includes(:user)
+#  end
 
- def create
-  @family_post = FamilyPost.create(family_post_params)
-  redirect_to family_posts_path
- end
+#  def create
+#   @family_post = FamilyPost.create(family_post_params)
+#   redirect_to family_posts_path
+#  end
 
- private
+#  private
 
- def family_post_params
-   params.require(:family_post).permit(:message, :image).merge(user_id: current_user.id)
- end
+#  def family_post_params
+#    params.require(:family_post).permit(:message, :image).merge(user_id: current_user.id)
+#  end
 
 end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :family_post]
+  
+  def show
+    @family_post = FamilyPost.new
+    @family_posts = @user.family_posts.order("created_at DESC")
+  end
+
+  def family_post
+    @user.FamilyPosts.create(family_post_params)
+    redirect_to user_path
+   end
+  
+   private
+
+   def family_post_params
+     params.require(:family_post).permit(:message, :image)
+   end
+
+   def set_user
+    @user = User.find(params[:id])
+   end
+
+end
