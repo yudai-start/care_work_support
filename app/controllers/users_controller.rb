5408834525fb3c1405c9668ba32cf484209fca9b
yudai <fukuta.yudai@gmail.com>
2020-01-10 19:30:45 +0900

    add-action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:family_room, :family_post]
   
+  def index
+  end
+
   def family_room
     @family_post = FamilyPost.new
     @family_posts = @user.family_posts.order("created_at DESC")
