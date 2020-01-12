3991c51a76b5ad66326ca3afc64ef421a82d1c81
yudai <fukuta.yudai@gmail.com>
2020-01-13 08:07:27 +0900

    add-family_post
    family_postコントローラーにindex、createアクションを追加。

--- app/controllers/family_posts_controller.rb
+++ app/controllers/family_posts_controller.rb
@@ -1,18 +1,24 @@
 class FamilyPostsController < ApplicationController
-#  def index
-#   @family_post = FamilyPost.new
-#   @family_posts = FamilyPost.where(user_id: current_user.id).includes(:user)
-#  end
+  before_action :set_family_room
 
-#  def create
-#   @family_post = FamilyPost.create(family_post_params)
-#   redirect_to family_posts_path
-#  end
+  def index
+    @family_post = FamilyPost.new
+    @family_posts = @family_room.family_posts.order("created_at DESC")
+  end
 
-#  private
+  def create   
+    @family_room.family_posts.create(family_post_params)
+    redirect_to family_room_family_posts_path
+  end
 
-#  def family_post_params
-#    params.require(:family_post).permit(:message, :image).merge(user_id: current_user.id)
-#  end
+  private
+
+  def family_post_params
+    params.require(:family_post).permit(:message, :image).merge(family_room_id: params[:family_room_id])
+  end
+
+  def set_family_room
+    @family_room = FamilyRoom.find(params[:family_room_id])
+  end
 
 end
