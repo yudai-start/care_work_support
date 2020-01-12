6f992aa0005749a42a0039c9a71e4ae2de3ae175
yudai <fukuta.yudai@gmail.com>
2020-01-13 08:09:06 +0900

    delete-family_room-from-user-controller
    ユーザーコントローラーにまとまっていたfamily_room、family_post関連のファイルを削除。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,29 +1,14 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:family_room, :family_post]
+  before_action :set_search, only: [:index]
   
   def index
+  end
+
+  private
+
+  def set_search
     @q = User.ransack(params[:q])
     @users = @q.result.where(role: "general").order("created_at DESC")
   end
-
-  def family_room
-    @family_post = FamilyPost.new
-    @family_posts = @user.family_posts.order("created_at DESC")
-  end
-
-  def family_post
-    @user.family_posts.create(family_post_params)
-    redirect_to family_room_user_path
-   end
-  
-   private
-
-   def family_post_params
-     params.require(:family_post).permit(:message, :image)
-   end
-
-   def set_user
-    @user = User.find(params[:id])
-   end
 
 end
