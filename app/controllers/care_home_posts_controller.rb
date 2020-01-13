c9daea1869f1876689cf681fac91f0d0b6ade292
yudai <fukuta.yudai@gmail.com>
2020-01-13 23:24:39 +0900

    add-care-home-post-search
    施設内の写真を、写っている人物の名前から検索する機能を実装

--- app/controllers/care_home_posts_controller.rb
+++ app/controllers/care_home_posts_controller.rb
@@ -3,7 +3,9 @@ class CareHomePostsController < ApplicationController
 
   def index
     @care_home_post = CareHomePost.new
-    @care_home_posts = CareHomePost.all.order("created_at DESC")
+
+    @q = CareHomePost.ransack(params[:q])
+    @care_home_posts = @q.result.order("created_at DESC")
   end
 
   def create
@@ -11,6 +13,7 @@ class CareHomePostsController < ApplicationController
 
     image_path = care_home_post_params[:image].original_filename #顔認証で必要となる投稿写真のファイル名のみを抽出
     users = search_all_users_in_photo(image_path)  #投稿された写真に写っている人物全員を特定
+    
     if users.present?
       save_user_care_home_posts(users, @care_home_post)  #userとcare_home_postの中間テーブルに、user_idとcare_home_post_idを保存
       send_mail(users) #各userの登録アドレスに通知メール送信
