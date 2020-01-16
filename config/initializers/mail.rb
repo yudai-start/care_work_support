06133fa3c8d0ad69fccc6a967b8c82705e11c62d
yudai <fukuta.yudai@gmail.com>
2020-01-16 21:05:07 +0900

    add-letter_opener-setting
    letter_opnerを使用するため、メール送信の設定を変更

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,10 +1,17 @@
-ActionMailer::Base.delivery_method = :smtp
-ActionMailer::Base.smtp_settings = {
-  address: 'smtp.gmail.com',
-  domain: 'gmail.com',
-  port: 587,
-  user_name: Rails.application.credentials[:mail][:user_name],
-  password: Rails.application.credentials[:mail][:password],
-  authentication: 'plain',
-  enable_starttls_auto: true
-}
+if Rails.env.production?
+  # ActionMailer::Base.delivery_method = :smtp
+  # ActionMailer::Base.smtp_settings = {
+  #   address: 'smtp.gmail.com',
+  #   domain: 'gmail.com',
+  #   port: 587,
+  #   user_name: Rails.application.credentials[:mail][:user_name],
+  #   password: Rails.application.credentials[:mail][:password],
+  #   authentication: 'plain',
+  #   enable_starttls_auto: true
+  # }
+  ActionMailer::Base.delivery_method = :letter_opener
+elsif Rails.env.development?
+  ActionMailer::Base.delivery_method = :letter_opener
+else
+  ActionMailer::Base.delivery_method = :test
+end
